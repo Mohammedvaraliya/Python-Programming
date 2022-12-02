f6a8894fdfa15b4a36f6e458c4ab1b8a2bd6fde3
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-02 22:51:14 +0530

    Add 05 python concept

---
+++ 05/main.py
@@ -0,0 +1,12 @@
+# Escape sequence characters
+print("Hey how are you")
+
+
+'''
+Multiline comment
+works like 
+this
+'''
+
+# The newline escape sequence character
+print("This is \n new line after the word new")
