7d177ef1628e37e1a291f2bfa70b114c033cb747
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-02 23:25:08 +0530

    Add 05 python concept

--- 05/main.py
+++ 05/main.py
@@ -10,3 +10,10 @@ this
 
 # The newline escape sequence character
 print("This is \n new line after the word new")
+
+# Multiple values in print statement
+print("Hello man", 8, 98, 76, "yeah")
+
+# separator in print statements
+print("Hey", 2, 4, "yes", sep="~", end="")
+print("Hello")
