1ffbc31d2977fec6cdea34f93d1509a882d428bf
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-03 23:58:52 +0530

    Add 06 python concept

---
+++ 06/main.py
@@ -0,0 +1,39 @@
+# Variables
+
+a = 1
+a1 = 91
+print(a)
+
+b = "Varaliya"
+print(b)
+
+print(a+a1)
+
+# To knoww the type of data type
+print("The type of variable a is", type(a))
+print("The type of variable b is", type(b))
+print("The type of variable a1 is", type(a1))
+
+c = 1.1
+print("The type of variable c is", type(c))
+
+d = complex(2, 4)
+print(d)
+print("The type of variable d is", type(d))
+
+# Boolean data types
+e = True
+print("The type of variable e is", type(e))
+
+# Sequence data type
+f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ["hello"], "yeah"]
+print(f)
+print("The type of variable f is", type(f))
+
+g = ("hello", 9, 3)
+print(g)
+print("The type of variable g is", type(g))
+
+h = {"name": "varaliya", "age": 21, "canVote": True}
+print(h)
+print("The type of variable h is", type(h))
