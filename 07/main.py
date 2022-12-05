cf3977acd9bf69aad284e2e0bb340be0a17ba1a8
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-05 18:39:57 +0530

    Add 08 python concept

--- 07/main.py
+++ 07/main.py
@@ -1,17 +1,7 @@
-# print(5+6)
-# print(15-6)
-# print(15*6)
-# print(15/6)
-# print(15//6)
-# print(5%3)
-# print(2**4)
-
-# Simple Calculator
-print("A Simple Calculator")
-a = 50
-b = 3
-
-print("The value of", a, "+", 3, "is: ", a + b)
-print("The value of", a, "-", 3, "is: ", a - b)
-print("The value of", a, "*", 3, "is: ", a * b)
-print("The value of", a, "/", 3, "is: ", a / b)
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
---
+++ 08/main.py
@@ -0,0 +1,9 @@
+# Simple Calculator
+print("A Simple Calculator")
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
