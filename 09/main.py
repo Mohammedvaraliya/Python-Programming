41541bdaf695f4cfb618d8932245c344a6ccaf0a
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-06 23:24:35 +0530

    Add 09 python type casting

---
+++ 09/main.py
@@ -0,0 +1,40 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d)
+
+e = "1.22"
+f = float(e)
+print(f)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# Example of explicit typecasting:
+string = "15"
+number = 7
+string_number = int(string) #throws an error if the string is not a valid integer
+sum= number + string_number
+print("The Sum of both the numbers is: ", sum)
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# Example of implicit type casting:
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))
+ 
+# Python automatically converts b to float
+b = 3.0
+print(type(b))
+ 
+# Python automatically converts c to float as it is a float addition
+c = a + b
+print(c)
+print(type(c))
