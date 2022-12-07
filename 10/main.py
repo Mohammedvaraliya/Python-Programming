10b8320cd079d17846bf38e121b3d8af84a28dd3
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-07 19:36:37 +0530

    Add 10 python "Taking user Input"

---
+++ 10/main.py
@@ -0,0 +1,13 @@
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x  + y)
+
+print(int(x) + int(y))
+
+# To take an integer input from user
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(x  + y)
