5ff8e85a2c3d2c0d2fa61708a5bf392ca3356e6a
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-11 23:35:03 +0530

    Add 14 python "If-Else Conditional statements"

---
+++ 14/main.py
@@ -0,0 +1,17 @@
+a = input("Enter Your name : ")
+b = int(input("Enter Your age : "))
+print(f"Your name is {a}, and Your age is : ", b)
+
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(b>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("You cannot drive")
+  print("No")
+  print("Yay!")
