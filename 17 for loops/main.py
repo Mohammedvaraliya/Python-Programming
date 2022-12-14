6f6067fc3df173f2944636e2aa4e84ff31a66132
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-14 17:49:36 +0530

    Restruct python files

---
+++ 17 for loops/main.py
@@ -0,0 +1,22 @@
+str1 = "Varaliya Mohammed"
+
+# for i in str1:
+#     print(i)
+#     if (i == "a"):
+#         print("This is a")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for k in range(5):
+#   print(k + 1)
+  
+# for k in range(1, 10):
+#   print(k)
+
+  
+for k in range(1, 12, 2):
+  print(k)
