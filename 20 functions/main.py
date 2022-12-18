2b398496d18607f0632e895012a32124945875f9
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-18 12:31:05 +0530

    Add 20  "Functions in python"

---
+++ 20 functions/main.py
@@ -0,0 +1,36 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def findgreater(a, b):
+    if(a > b):
+        print("Fist number is greater")
+    else:
+        print("Second number is greater")
+
+def isLesser(a, b):
+    pass
+
+    
+
+a = 9
+b = 81
+
+# if(a > b):
+#     print("Fist number is greater")
+# else:
+#     print("Second number is greater")
+
+# gmean = (a*b)/(a+b)
+# print(gmean)
+findgreater(a, b)
+calculateGmean(a, b)
+
+c = 8
+d = 7
+
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+findgreater(c, d)
+calculateGmean(c, d)
+
