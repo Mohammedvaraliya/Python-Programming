7f850c39663f4c084c3a0eb50c94408a0612a81e
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-26 22:58:16 +0530

    Add 28  "DocStrings and Pep8"

---
+++ 29 docstrings/main.py
@@ -0,0 +1,37 @@
+# DocStrings
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+
+
+square(5)
+print(square.__doc__)
+
+# Pep8
+import this
+
+# comment
+# after import this the python will print this information
+'''
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
