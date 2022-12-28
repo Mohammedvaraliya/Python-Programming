f8e4d96e4a8b7bc948b33d024660453c829a2eed
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-28 17:55:59 +0530

    Add 31  "Set Data Type"

---
+++ 31 set data type/main.py
@@ -0,0 +1,19 @@
+s = {2, 3, 4, 5, 6, 7, 7, 8}
+print(s)
+
+info = {"Carla", "Samuel", True, 9.99, 10, 19, 10}
+print(info)
+
+# to make the empty set variable
+vara = {}
+print(type(vara)) #it will print the dict
+
+# To declare an empty variable with set datatype
+vara = set()
+print(type(vara))
+
+# To print the elements of set
+info = {"Carla", "Samuel", True, 9.99, 10, 19, 10}
+
+for value in info:
+    print(value)
