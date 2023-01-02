92b9f794581db705316075536cea1ba178bd3497
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-01-02 15:12:29 +0530

    Add 35  "for Loop with else"

---
+++ 35 for Loop with else/main.py
@@ -0,0 +1,35 @@
+# for i in range(6):
+#     print(i)
+# else:
+#     print("Sorry")
+
+# if condition fails because list is empty
+# for i in []:
+#     print(i)
+# else:
+#     print("Sorry")
+
+
+# for i in range(6):
+#     print(i)
+#     if i == 4:
+#         break
+# else:
+#     print("Sorry")
+
+
+i = 0
+while i<7:
+  print(i)
+  i = i + 1
+  # if i == 4:
+  #   break
+
+else:
+  print("Sorry no i")
+
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
