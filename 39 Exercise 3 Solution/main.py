383b71d721e3a20b49e267cf42d9b14bffb03ca8
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-01-05 18:20:04 +0530

    Add 39  "Exercise 3 Solution"

--- 39 Exercise 3 Solution/main.py
+++ 39 Exercise 3 Solution/main.py
@@ -1,71 +1,124 @@
-print("Welcome to the varaliya KBC(Kon Banega CrorePati)")
-
 questions = [
-    "The International Literacy Day is observed on",
-    "The language of Lakshadweep. a Union Territory of India, is",
-    "In which group of places the Kumbha Mela is held every twelve years?",
-    "Bahubali festival is related to",
-    "Which day is observed as the World Standards  Day?"
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
 ]
 
-answers = [
-    [
-        "1. Sep 8",
-        "2. Nov 28",
-        "3. May 2",
-        "4. Sep 22"
-    ],
-    [
-        "1. Tamil",
-        "2. Hindi",
-        "3. Malayalam",
-        "4. Telugu"
-    ],
-    [
-        "1. Ujjain. Purl; Prayag. Haridwar",
-        "2. Prayag. Haridwar, Ujjain,. Nasik",
-        "3. Rameshwaram. Purl, Badrinath. Dwarika",
-        "4. Chittakoot, Ujjain, Prayag,'Haridwar"
-    ],
-    [
-        "1. Islam",
-        "2. Hinduism",
-        "3. Buddhism",
-        "4. Jainism"
-    ],
-    [
-        "1. June 26",
-        "2. Oct 14",
-        "3. Nov 15",
-        "4. Dec 2"
-    ]
-]
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+for i in range(0, len(questions)):
+  
+  question = questions[i]
+  print(f"\n\nQuestion for Rs. {levels[i]}")
+  print(f"a. {question[1]}          b. {question[2]} ")
+  print(f"c. {question[3]}          d. {question[4]} ")
+  reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+  if (reply == 0):
+    money = levels[i-1]
+    break
+  if(reply == question[-1]):
+    print(f"Correct answer, you have won Rs. {levels[i]}")
+    if(i == 4):
+      money = 10000
+    elif(i == 9):
+      money = 320000
+    elif(i == 14):
+      money = 10000000
+  else:
+    print("Wrong answer!")
+    break 
 
-correct_ans = [1, 3, 2, 4, 2]
-
-final_amt = 0
-i = 0
-name = input("Enter Your name : ")
-while i < len(questions):
-    print("\n")
-    print(f"Your balance is {final_amt}")
-    print("Please select the correct option and win 10000")
-    print("\n")
-    print(questions[i])
-    print("Answers:")
-    print(*answers[i], sep="\n")
-    print("\n")
-    user_answer = int(input("Enter the correct option and win prize : "))
-    print("\n")
-    
-    print("The correct answer is : ", correct_ans[i])
-    
-    if user_answer == correct_ans[i]:
-        print("Congratulation you've successfully won the prize")
-        final_amt += 10000
-        i += 1
-    else:
-        i += 1
-    
-print(f"{name} Your Final amount after winning the questions is : {final_amt}")
-    
+print(f"Your take home money is {money}")
