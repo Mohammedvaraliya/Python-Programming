b74763c147190f54129bcd946dc72415b15b8645
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-01-08 00:04:20 +0530

    Add 41  "One line if else statement, with 3 conditions:"

---
+++ 41 short hand if else statements/main.py
@@ -0,0 +1,13 @@
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# One line if else statement, with 3 conditions:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# One line if else statement, else is necessary otherwise it will throw error
+a = 50
+b = 33
+print("a i greater than b") if a > b else print("")
