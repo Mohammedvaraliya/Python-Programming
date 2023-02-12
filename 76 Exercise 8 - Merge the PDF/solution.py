8ec68750182384192fb5a007da9d8791208d3aa8
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-02-12 12:07:16 +0530

    Add 76 Exercise 8 - Merge the PDF

---
+++ 76 Exercise 8 - Merge the PDF/solution.py
@@ -0,0 +1,17 @@
+from PyPDF2 import PdfWriter
+
+merger = PdfWriter()
+
+pdf_files = [
+    "76 Exercise 8 - Merge the PDF\pdf files\Research Synopsis (S091).pdf",
+    "76 Exercise 8 - Merge the PDF\pdf files\Research Synopsis (S096).pdf",
+    "76 Exercise 8 - Merge the PDF\pdf files\Research Synopsis (S105).pdf",
+    "76 Exercise 8 - Merge the PDF\pdf files\Research Synopsis (S106).pdf",
+    "76 Exercise 8 - Merge the PDF\pdf files\Research Synopsis (S110).pdf"
+    ]
+
+for pdf in pdf_files:
+    merger.append(pdf)
+
+merger.write("76 Exercise 8 - Merge the PDF\Merge-pdf.pdf")
+merger.close()
