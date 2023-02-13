7eaeb4c3cb6d86ea65afb6dcb50708309afc0604
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-02-13 18:44:42 +0530

    Add 78 Single Inheritance

---
+++ 78 Single Inheritance/main.py
@@ -0,0 +1,41 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+class Cat(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="cat")
+        self.breed = breed
+
+    def make_sound(self):
+        print("Meow")
+
+    def cat_speciality(self):
+        print("This cat can protect you with thief")
+
+
+
+
+a = Animal("Animal", "AnimalMan")
+a.make_sound()
+
+d = Dog("Dog", "DoggerMan")
+d.make_sound()
+
+c = Cat("Cat", "CatMan")
+c.make_sound()
+c.cat_speciality()
+
+
